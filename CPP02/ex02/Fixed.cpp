331551147ba07f33eff6a5917a556f615459c04a
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-01-13 16:23:18 +0400

    completed ex02

---
+++ CPP02/ex02/Fixed.cpp
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esimpson <esimpson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 16:22:22 by esimpson          #+#    #+#             */
+/*   Updated: 2025/01/13 16:22:35 by esimpson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void):_fixedNumValue(0){}
+
+Fixed::Fixed(const int intVal)
+{
+    _fixedNumValue = intVal  << _fractionBits;
+}
+
+Fixed::Fixed(const float floatVal)
+{
+    _fixedNumValue = roundf(floatVal * (1 << _fractionBits));
+}
+
+Fixed::Fixed(const Fixed& fix)
+{
+      _fixedNumValue = fix._fixedNumValue;
+}
+
+Fixed& Fixed::operator=(const Fixed &fix)
+{
+    if(this != &fix)
+         _fixedNumValue = fix._fixedNumValue;      
+    return(*this);
+}
+
+Fixed::~Fixed(){}
+
+int Fixed::getRawBits(void) const
+{
+    return(_fixedNumValue);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    this->_fixedNumValue = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+     return((float) (_fixedNumValue) / (1 << _fractionBits));    
+}
+ 
+int Fixed::toInt( void ) const
+{
+    return ( _fixedNumValue >> _fractionBits);
+}
+
+//overloading insertion operator
+std::ostream &operator<<(std::ostream &out, Fixed const &fix)
+{
+    out << fix.toFloat();
+    return(out);
+}
+
+//overloaded comparison operators
+bool Fixed::operator> (const Fixed& fix)const
+{
+    return(this->_fixedNumValue > fix._fixedNumValue);
+}
+
+bool Fixed::operator< (const Fixed& fix)const
+{
+    return(this->_fixedNumValue < fix._fixedNumValue);
+}
+
+bool Fixed::operator>= (const Fixed& fix)const
+{
+    return(this->_fixedNumValue >= fix._fixedNumValue);
+}
+
+bool Fixed::operator<= (const Fixed& fix)const
+{
+    return(this->_fixedNumValue <= fix._fixedNumValue);
+}
+
+bool Fixed::operator== (const Fixed& fix)const
+{
+    return(this->_fixedNumValue == fix._fixedNumValue);
+}
+
+bool Fixed::operator!= (const Fixed& fix)const
+{
+    return(this->_fixedNumValue != fix._fixedNumValue);
+}
+
+//overloaded arithmetic operators
+Fixed Fixed::operator+ (const Fixed &fix)const
+{
+    Fixed result;
+    result._fixedNumValue = this->_fixedNumValue + fix._fixedNumValue;
+    return (result);
+}
+
+Fixed Fixed::operator- (const Fixed &fix)const
+{
+    Fixed result;
+    result._fixedNumValue = this->_fixedNumValue - fix._fixedNumValue;
+    return (result);
+}
+
+Fixed Fixed::operator* (const Fixed &fix)const
+{
+    Fixed result;
+    result._fixedNumValue = this->_fixedNumValue * fix.toFloat();
+    return (result);
+}
+
+Fixed Fixed::operator/ (const Fixed &fix)const
+{
+    Fixed result;
+    result._fixedNumValue = this->_fixedNumValue / fix.toFloat() ;
+    return (result);     
+   
+}
+
+//overload prefix operators
+Fixed &Fixed::operator++(void)
+{
+    this->_fixedNumValue++;
+    return(*this);
+}
+
+Fixed &Fixed::operator--(void)
+{
+    this->_fixedNumValue--;
+    return(*this);
+}
+
+//overload postfix operators
+Fixed Fixed::operator++(int)
+{
+    Fixed tmp;
+    tmp._fixedNumValue = this->_fixedNumValue++;
+    return(tmp);
+}
+
+Fixed Fixed::operator--(int)
+{
+    Fixed tmp;
+    tmp._fixedNumValue= this->_fixedNumValue--;
+    return(tmp);
+}
+
+//static member functions
+Fixed& Fixed::min(Fixed& nbr1, Fixed& nbr2)
+{
+    return(nbr1 < nbr2) ? nbr1 : nbr2;
+}
+ 
+const Fixed& Fixed::min(const Fixed& nbr1, const Fixed& nbr2)
+{
+    return(nbr1 < nbr2) ? nbr1 : nbr2;
+}
+ 
+Fixed& Fixed::max(Fixed& nbr1, Fixed& nbr2)
+{
+    return(nbr1 > nbr2) ? nbr1 : nbr2;
+}
+
+const Fixed& Fixed::max(const Fixed& nbr1, const Fixed& nbr2)
+{
+    return(nbr1 > nbr2) ? nbr1 : nbr2;
+}
---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esimpson <esimpson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 15:34:42 by esimpson          #+#    #+#             */
+/*   Updated: 2025/01/13 16:21:00 by esimpson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed const b( Fixed( 5.05f )  * Fixed(2));
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+return 0;
+}
