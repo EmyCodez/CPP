13724938c8d27ff0ce84b50ecaf3b41219e46b52
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-01-26 19:30:45 +0400

    ex03 incomplete

---
+++ CPP04/ex03/AMateria.hpp
@@ -0,0 +1,29 @@
+# pragma once
+# ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+#include <iostream>
+
+class ICharacter;
+
+class AMateria
+{
+
+    protected:
+    std::string _type;
+
+    public:
+    AMateria(void);
+    AMateria(std::string const &type);
+    AMateria(const AMateria &copy);
+    AMateria &operator= (const AMateria &copy);
+    virtual ~AMateria();
+
+    //member functions
+    std::string const & getType() const; //Returns the materia type
+    virtual AMateria* clone() const = 0;
+    virtual void use(ICharacter& target);
+
+};
+
+# endif
